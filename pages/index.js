5d7db5ccf79071afa07fdaaa9b899412fc685be7
nadans <nisrinaseptiana026@gmail.com>
2023-05-31 14:52:37 +0700

    feature(PP-ID): add ga4

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 /* eslint-disable */
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import ListArticle from "../src/components/Article/listArticle";
 import { Suspense } from "react";
 import FilterArticle from "../src/components/Article/filterArticle";
 import { Box, CircularProgress } from "@mui/material";
-import { useState } from "react";
 import loadable from '@loadable/component'
 import AppBarComponent from "../src/components/Appbar/appbar";
+import Script from "next/script";
 const LisArticleComponent = loadable(() => import('../src/components/Article/listArticle'))
 export default function Home() {
 
@@ -15,6 +14,23 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <Script
+              type="text/javascript"
+              async
+              dangerouslySetInnerHTML={{
+                __html: `
+              (function(w, d, s, l, i) {
+                    w[l] = w[l] || []
+                    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+                    const f = d.getElementsByTagName(s)[0],
+                      j = d.createElement(s),
+                      dl = l != "dataLayer" ? "&l=" + l : ""
+                    j.async = true
+                    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+                    f.parentNode.insertBefore(j, f)
+                  })(window,document,'script','dataLayer',"GTM-MCWQ6MS")`,
+              }}
+            />
       </Head>
 
       <main className={styles.main}>
@@ -25,7 +41,7 @@ export default function Home() {
 
         <Suspense
           fallback={
-            <div style={{ width: "100%", height: "100%" }}>
+            <div styles={{ width: "100%", height: "100%" }}>
               <CircularProgress color="inherit" />
             </div>
           }
